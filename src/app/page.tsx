1c3e19baa829ffb9a65ee2b247ad9a79628669ee
Ugo Laurit <u.laurit@gmail.com>
2023-12-28 20:07:37 +0100

    refactor: arrow functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,26 @@ import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
-export default async function Home() {
+async function CrudShowcase() {
+  const session = await getServerAuthSession();
+  if (!session?.user) return null;
+
+  const latestPost = await api.post.getLatest.query();
+
+  return (
+      <div className="w-full max-w-xs">
+        {latestPost ? (
+            <p className="truncate">Your most recent post: {latestPost.name}</p>
+        ) : (
+            <p>You have no posts yet.</p>
+        )}
+
+        <CreatePost />
+      </div>
+  );
+}
+
+const Home = async () => {
   const hello = await api.post.hello.query({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
@@ -62,21 +81,4 @@ export default async function Home() {
   );
 }
 
-async function CrudShowcase() {
-  const session = await getServerAuthSession();
-  if (!session?.user) return null;
-
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
-  );
-}
+export default Home;
