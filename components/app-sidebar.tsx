8ba69eb55acd0707f07bc7e679d1c9cfd2c9259f
Ugo Laurit <u.laurit@gmail.com>
2024-12-03 12:54:46 +0100

    feat: add sidebar

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,72 @@
+import {
+  CreditCard,
+  Landmark,
+  LayoutDashboard,
+  Scale,
+  Settings,
+} from 'lucide-react'
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+
+const items = [
+  {
+    title: 'Dashboard',
+    url: '#',
+    icon: LayoutDashboard,
+  },
+  {
+    title: 'Accounts',
+    url: '#',
+    icon: Landmark,
+  },
+  {
+    title: 'Transactions',
+    url: '#',
+    icon: CreditCard,
+  },
+  {
+    title: 'Budgets',
+    url: '#',
+    icon: Scale,
+  },
+  {
+    title: 'Settings',
+    url: '#',
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Picsou</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
